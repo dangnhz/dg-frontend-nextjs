4f2d33b66b268062e4b639a46255de0cf765d9ed
Danny Ngo <danny@digitalgarden.com.au>
2022-09-23 11:23:34 +1000

    implemented redirects config at build time

--- next.config.js
+++ next.config.js
@@ -1,19 +1,38 @@
 /** @type {import('next').NextConfig} */
+
 const path = require('path')
+
+const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL
+
+async function fetchRedirects() {
+  return fetch(`${BASE_API_URL}/redirects`)
+    .then((res) => res.json())
+    .then((data) => {
+      return data
+    })
+}
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["api.digitalgarden.com.au", "api.dev-54ta5gq-gr52polju7uks.au.platformsh.site", "api.2022-services-update-mhelslq-gr52polju7uks.au.platformsh.site"],
+    domains: [
+      'api.digitalgarden.com.au',
+      'api.dev-54ta5gq-gr52polju7uks.au.platformsh.site',
+      'api.2022-services-update-mhelslq-gr52polju7uks.au.platformsh.site',
+    ],
   },
 
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "styles.scss";`
-  }
+    prependData: `@import "styles.scss";`,
+  },
 
-};
+  async redirects() {
+    const siteRedirects = await fetchRedirects()
 
-module.exports = nextConfig;
+    return siteRedirects
+  },
+}
 
+module.exports = nextConfig
